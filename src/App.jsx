148c7cb7578a8ed44b8913dcc32a418a7e2f2371
Vishal Sharma <vsvishal11@yahoo.com>
2023-11-10 20:38:44 +0530

    appwrite services updated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import conf from "./conf/conf";
+
+function App() {
+  console.log(conf.appwriteUrl);
+
+  return (
+    <>
+      <h1>Hello Vishal</h1>
+    </>
+  );
+}
+
+export default App;
